9909349797ad3aa7c484c68a799d80ff2e970c72
Xavi R. Pinteño <xavi@jrpinteno.me>
2018-04-21 12:18:50 +0300

    First rts commit

---
+++ Assets/Scripts/rts/Unit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+	public int life;
+	public int damage;
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
