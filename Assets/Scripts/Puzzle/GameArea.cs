84382556114b2a12fe931d9cef6ce5bb15bddcdc
Nacho Lasheras <fanatiko@gmail.com>
2018-04-21 12:09:23 +0300

    Some starting of the puzzle part

---
+++ Assets/Scripts/Puzzle/GameArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameArea : MonoBehaviour {
+
+	void Start() {
+		
+	}
+	
+	void Update() {
+		
+	}
+
+	public void tick() {
+
+	}
+
+	public bool isGrounded(Tetrimino piece) {
+		return false;
+	}
+
+	public void getBlocks(Tetrimino piece) {
+
+	}
+}
---
+++ Assets/Scripts/Puzzle/TetrisGame.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisGame : MonoBehaviour
+{
+	private Tetrimino nextPiece;
+	private Tetrimino currentPiece;
+
+	private GameArea gameArea;
+
+	public float speed = 1.0f;
+
+	private float tickTime;
+
+	public GameObject blockPrefab;
+
+	void Start() {
+		nextPiece = getRandomPiece();
+		currentPiece = null;
+
+		gameArea = new GameArea();
+
+		tickTime = speed;
+	}
+	
+	void Update() {
+		if (currentPiece == null) {
+			currentPiece = nextPiece;
+			nextPiece = getRandomPiece();
+		}
+
+		tickTime -= Time.deltaTime;
+
+		if (tickTime < 0.0f)
+		{
+			tick();
+			tickTime = speed;
+		}
+
+	}
+
+	private Tetrimino getRandomPiece() {
+		
+		return new Tetrimino();
+	}
+
+	private void tick() {
+		currentPiece.tick();
+
+		if (gameArea.isGrounded(currentPiece))
+		{
+			gameArea.getBlocks(currentPiece);
+			GameObject.Destroy(currentPiece);
+			currentPiece = null;
+		}
+
+		gameArea.tick();
+	}
+}
